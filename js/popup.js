2ed5b276da0587489c6b247533d9902921be105f
ingvarcap <igorilin1984@inbox.ru>
2019-03-23 08:37:07 +0400

    js

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,26 @@
 var link=document.querySelector(".query_form");
 var popup=document.querySelector(".modal_login");
 var close=document.querySelector(".modal_close");
+var login=document.querySelector("[name=name]");
+var form=document.querySelector(".modal_login");
+var email=document.querySelector("[name=mail]");
+var textarea=document.querySelector("textarea");
+
+form.addEventListener("submit", function(evt) {
+	evt.preventDefault();
+	console.log(login.value);
+	console.log(email.value);
+	console.log(textarea.value);
+});
 
 link.addEventListener("click", function(evt) {
-evt.preventDefault();
-popup.classList.add("modal_show");
+	evt.preventDefault();
+	popup.classList.add("modal_show");
+	login.focus();
 });
 
 close.addEventListener("click", function(evt) {
-evt.preventDefault();
-console.log("нормально");
-popup.classList.remove("modal_show");
+	evt.preventDefault();
+	popup.classList.remove("modal_show");
 });
+
